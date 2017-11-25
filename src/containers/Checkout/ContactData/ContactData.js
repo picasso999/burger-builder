edd475a6cd61bac4c14a5e624fbaa4947e80ce42
Puranam Pradeep Picasso <pichupicasso@gmail.com>
2017-11-25 10:29:07 +0530

    first commit for React-Redux-Firebase-SPA-app

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,217 @@
+import React, {Component} from "react";
+import {connect } from 'react-redux';
+
+import Button from "../../../components/UI/Button/Button";
+import Spinner from "../../../components/UI/Spinner/Spinner";
+import classes from "./ContactData.css";
+import axios from "../../../axios-orders";
+import Input from "../../../components/UI/Input/Input";
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorhandler';
+import * as actions from '../../../store/actions/index';
+
+class ContactData extends Component {
+    state = {
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+            },
+            ZipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIPCode'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 6
+                },
+                valid: false,
+                touched: false
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Country'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: 'fastest',
+                validation: {},
+                valid: true
+            }
+        },
+        formIsValid: false
+        // loading: false
+    };
+
+    orderHandler = event => {
+        event.preventDefault();
+
+        this.setState({loading: true});
+        const formData = {};
+        for(let formElementIndentifier in this.state.orderForm){
+            formData[formElementIndentifier] = this.state.orderForm[formElementIndentifier].value;
+        }
+        const order = {
+            ingredients: this.props.ings,
+            price: this.props.price,
+            orderData: formData,
+            userId: this.props.userId
+        };
+
+        this.props.onOrderBurger(order, this.props.token);
+        // axios
+        //     .post("/orders.json", order)
+        //     .then(response => {
+        //         this.setState({loading: false});
+        //         console.log("sent to firebase");
+        //         this
+        //             .props
+        //             .history
+        //             .push("/");
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //         this.setState({loading: false});
+        //     });
+        console.log(this.props.ings);
+    };
+
+    checkValidity(value,rules) {
+        let isValid = true;
+        if(rules.required){
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIndentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIndentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value,updatedFormElement.validation);
+        updatedFormElement.touched = true;
+        updatedOrderForm[inputIndentifier] = updatedFormElement;
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
+    }
+
+    render() {
+        const formElementsArray = [];
+        for(let key in this.state.orderForm){
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+        let form = (
+
+            <form onSubmit={this.orderHandler} >
+                
+                {formElementsArray.map(formElement => {
+                    return (
+                        <Input key={formElement.id} elementType={formElement.config.elementType} elementConfig={formElement.config.elementConfig} value={formElement.config.value} changed={(event) => this.inputChangedHandler(event,formElement.id)} invalid={!formElement.config.valid} shouldValidate={formElement.config.validation} touched={formElement.config.touched} />
+                    )
+                })}
+                <Button btnType="Success" disabled={!this.state.formIsValid} >
+                    ORDER{" "}
+                </Button>
+            </form>
+        );
+        if (this.props.loading) {
+            form = <Spinner/>;
+        }
+        return (
+            <div className={classes.ContactData}>
+                <h4>Enter your contact data</h4>
+                {form}
+            </div>
+        );
+    }
+};
+
+const mapStateToProps = state => {
+    return {
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading,
+        token: state.auth.token,
+        userId: state.auth.userId
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onOrderBurger: (orderData, token) => dispatch(actions.purchaseBurger(orderData, token))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import {connect} from 'react-redux';
+
+import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorhandler';
+import * as actions from '../../store/actions/index';
+// import Spinner from '../../components/UI/Spinner/Spinner';
+
+class Orders extends Component {
+
+    // state={
+    //     orders: [],
+    //     loading: true
+    // }
+
+    componentDidMount(){
+        this.props.onFetchOrders(this.props.token, this.props.userId);
+        // axios.get('./orders.json').then(res => {
+        //     const fetchOrders = [];
+        //     for( let key in res.data){
+        //         fetchOrders.push({
+        //             ...res.data[key],
+        //             id: key
+        //         })
+        //     }
+        //     this.setState({loading: false, orders: fetchOrders});
+        // }).catch(err => {
+        //     this.setState({loading: false});
+        // })
+    }
+    render() {
+        
+        // if(this.props.loading){
+        //     let orders = (
+                
+        //     )
+        // }
+        return (
+            <div>
+            {this.props.orders.map(order => {
+                return (<Order key={order.id} ingredients={order.ingredients} price={order.price} />)
+            })}
+            </div>
+        )
+    }
+};
+
+const mapStateToProps = state => {
+    return {
+        orders: state.order.orders,
+        loading: state.order.loading,
+        token: state.auth.token,
+        userId: state.auth.userId
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
+    }
+};
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
